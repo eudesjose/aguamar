4f373b03a89acca547b642c4342c9e36966feb90
eudesjose <eudesjose1@gmail.com>
2019-12-07 11:43:52 -0300

    Correção do commit

--- src/main/java/com/distribuidora/aguamar/repositorio/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.distribuidora.aguamar.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.distribuidora.aguamar.modelos.Role;
-
-public interface RoleRepository extends CrudRepository<Role, String>{
-
-}
